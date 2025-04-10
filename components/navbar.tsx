3a9e8d7f6aee6ba20884d4218ddbc5a6bdabc484
SokolikSerj <sokolikserg@gmail.com>
2025-04-10 14:54:08 +0300

    1

---
+++ components/navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link"
+
+export const Navbar = () => {
+    return (
+        <nav className="sticky top-o z-50 bg-white shadow">
+            <div className="container mx-auto flex items-center justify-between px-4 py-4">
+                <Link href="/" className="hover:text-blue-600">
+                    My ecommerce
+                </Link>
+                <div className="hidden md:flex space-x-6">
+                    <Link href="/">Home</Link>
+                    <Link href="/products" className="hover:text-blue-600">Products</Link>
+                    <Link href="/checkout" className="hover:text-blue-600">Checkout</Link>
+                </div>
+
+                <div className="flex items-center space-x-4"></div>
+            </div>
+        </nav>
+    )
+}
